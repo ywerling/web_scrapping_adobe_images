724245a9afb3c8830c4ea8297be3ebc421ae765e
ywerling <ywerling@hotmail.de>
2024-05-16 10:20:31 +0200

    Save image description to text file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 ADOBE_STOCK_IMAGES_URL = "https://stock.adobe.com/"
-OUTPUT_FILE = 'output/output.txt'
+OUTPUT_DESTINATION = 'output.txt'
 
 # SEARCH_TERM = "Nature"
 search_term = input('Enter the search keywords to find images: ')
@@ -18,10 +18,26 @@ chrome_options.add_experimental_option("detach", True)
 driver=webdriver.Chrome(chrome_options)
 driver.get(ADOBE_STOCK_IMAGES_URL)
 
-#allow the page to load
+# allow the page to load
 time.sleep(1)
 
 search_input = driver.find_element(By.NAME,"keyword")
 # search_input.send_keys(SEARCH_TERM)
 search_input.send_keys(search_term)
 search_input.send_keys(Keys.ENTER)
+
+# allow the page to load
+time.sleep(1)
+
+# images = driver.find_elements(By.CSS_SELECTOR, "#js-img-protect alt")
+images = driver.find_elements(By.XPATH, "//img[@alt]")
+for image in images:
+    # print(image.get_attribute("alt"))
+    # print(image.get_attribute("class"))
+    print(f'{image.get_attribute("alt")}{image.get_attribute("class")}\n')
+
+with open(OUTPUT_DESTINATION, "w", encoding='utf-8') as output_file:
+    for image in images:
+        text_to_write = image.get_attribute("alt")
+        output_file.write(text_to_write)
+        output_file.write('\n')

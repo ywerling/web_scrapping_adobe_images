eab430f6a513a4c986100a80e4e9f9639803968e
ywerling <ywerling@hotmail.de>
2024-05-16 10:46:51 +0200

    Slice image list to remove irrelevant components

--- main.py
+++ main.py
@@ -30,14 +30,21 @@ search_input.send_keys(Keys.ENTER)
 time.sleep(1)
 
 # images = driver.find_elements(By.CSS_SELECTOR, "#js-img-protect alt")
+# get images from the webpage
 images = driver.find_elements(By.XPATH, "//img[@alt]")
+
+# this part is for testing purpose only, it has to be commented out for production
 for image in images:
     # print(image.get_attribute("alt"))
     # print(image.get_attribute("class"))
-    print(f'{image.get_attribute("alt")}{image.get_attribute("class")}\n')
+    print(f'{image.get_attribute("alt")}{image.get_attribute("name")}\n')
 
+# write image titles to destination file
+# UTF-8 to support unicode characters
 with open(OUTPUT_DESTINATION, "w", encoding='utf-8') as output_file:
-    for image in images:
+    #slicing to remove first and last images which are not relevant to the search
+    for image in images[3:-3]:
         text_to_write = image.get_attribute("alt")
-        output_file.write(text_to_write)
-        output_file.write('\n')
+        if len(text_to_write) > 1:
+            output_file.write(text_to_write)
+            output_file.write('\n')
